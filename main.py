b31ffab56b2e35af0f40f5be09327112769b6f83
armonperez <armonperez16@gmail.com>
2024-09-02 23:13:03 -0700

    small spaces to the user input

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
 while True:
 
     random_number = randint(1,10)
-    guess = int(input("Please enter a number 1-10"))
+    guess = int(input("Please enter a number 1-10: " " "))
     if guess == random_number:
 
         print ("You win!")
@@ -18,7 +18,7 @@ while True:
 
     else:
         
-        print (f"you lost the correct number was {random_number}")
+        print (f"you lost the correct number was {random_number}.")
 
 
 main()
